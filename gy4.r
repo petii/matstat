d466e142f5077dfc50a943e43346f8d2b1581272
Vida Péter <petike997@gmail.com>
2018-03-13 01:06:06 +0100

    Added stuff

--- gy4.r
+++ gy4.r
@@ -6,19 +6,35 @@
 #surusegfv becslesnel: konvergencia vizsgalata 
 n=c(10,40,160,640)
 est=matrix(0,100,4)
-for(j in 1:100)
-{
-  for (i in 1:length(n))
-  {x=rnorm(n[i])
-  yy=density(x,adjust=1.2)
-  yd=yy$y
-  xd=yy$x
-  est[j,i]=max(abs(yd-dnorm(xd)))}
+for(j in 1:100) {
+  for (i in 1:length(n)) {
+    x=rnorm(n[i])
+    yy=density(x,adjust=1.2)
+    yd=yy$y
+    xd=yy$x
+    est[j,i]=max(abs(yd-dnorm(xd)))
+  }
 }
 
 #HF1:
 #a/ hogyan tudnánk ábrázolni?
+boxplot(est)
+
 #b/ nézzük meg különböző adjust értékekre és nézzük meg, melyik a legjobb
+n=10
+adj=c(0.5,1,1.2,1.5,2)
+est=matrix(0,100,length(adj))
+for(j in 1:100) {
+  #for (i in 1:length(n)) {
+  for (i in length(adj)) {
+    x=rnorm(n)
+    yy=density(x,adjust=adj[i])
+    yd=yy$y
+    xd=yy$x
+    est[j,i]=max(abs(yd-dnorm(xd)))
+  }
+}
+boxplot(est)
 
 ###
 #Cauchy pelda
